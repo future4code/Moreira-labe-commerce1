8320bada768adf0abc5d6ad331fa55ee9f8b023b
HEINRICK7 <heinrickcostta@gmail.com>
2022-01-19 20:51:14 -0300

    modifield pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Main from './components/Main'
-import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
      <Main />
+     <Footer />
     </div>
   );
 }
