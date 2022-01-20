b3fd50141bc8d12824d661fd137c8e22e8dd28a7
Carlos Henrique da Costa <heinrickcostta@gmail.com>
2022-01-19 22:06:26 -0300

    Revert "Developer arthur"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-
-import Header from './components/Header' 
+import React from 'react';
 import Main from './components/Main'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-     <Header /> 
+
      <Main />
      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
