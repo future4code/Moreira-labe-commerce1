a8b4812d505f2a4786d5a8c3074ea1789f373ae8
Arthut Kelvim <kelvimarthur@gmail.com>
2022-01-20 12:54:29 -0300

    Alteração finalizada: Mudada cor do Header da Home e feito Header da Pag Marketplace

---
+++ src/components/MarketplaceHeader/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ImagemLogo = styled.img`
+    width: 70px;
+` 
+export const Div = styled.div`
+    height: 80px;
+    background-color: #3f3d56;
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 40px;
+    align-items: center;
+`
+
--- src/pages/MarketPlace/index.js
+++ src/pages/MarketPlace/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MarketplaceHeader from '../../components/MarketplaceHeader';
 
 class MarketPlace extends Component {
     render() {
         return (
             <div>
-                hello Place
+                <MarketplaceHeader/>
             </div>
         );
     }
