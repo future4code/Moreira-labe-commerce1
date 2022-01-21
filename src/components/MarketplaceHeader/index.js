6cb7bda0a1af9453efee2f17772e9f1ba5d5fb31
HEINRICK7 <heinrickcostta@gmail.com>
2022-01-21 15:45:04 -0300

    update pages

--- src/components/MarketplaceHeader/index.js
+++ src/components/MarketplaceHeader/index.js
@@ -7,8 +7,6 @@ import LogoMarket from '../../assets/logospace.png'
 import { ImagemLogo } from './styled'
 import { Div } from './styled'
 
-import Modal from '../Modal/modal'
-
 class Index extends Component {
   render() {
     return (
